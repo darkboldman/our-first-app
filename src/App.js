4a045ec7ca99f1c710a66058f74f209ca591c1a0
darkboldman <palitha@outlook.com>
2021-08-03 12:31:21 +1000

    counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    // Incorrect way to apparoach ----> count += 1;
+    //Correct way to do increment is as below
+    setCount(count + 1)
+  };
+
+  const decrement = () => {
+    // Incorrect way to apparoach ----> count -= 1;
+    //Correct way to do increment is as below
+    setCount(count - 1)
+  };
+
   return (
     //BEM naming convention
     <div className="app">
-      <h1> Hello World</h1>
+      <h1> Welcome to my Counter App</h1>
+      <p>The count is: {count}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
